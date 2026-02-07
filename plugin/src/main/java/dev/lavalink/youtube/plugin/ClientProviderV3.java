beed09c67b78197ef773d9563374ead4517b8d0a
raziscofield <rajihraju11@gmail.com>
2026-02-07 23:51:46 +0530

    feat: youtube issue fixed , addded TVHTML5_SIMPLY client

--- plugin/src/main/java/dev/lavalink/youtube/plugin/ClientProviderV3.java
+++ plugin/src/main/java/dev/lavalink/youtube/plugin/ClientProviderV3.java
@@ -18,8 +18,8 @@ public class ClientProviderV3 implements ClientProvider {
         TV(Tv::new),
         WEB(Web::new),
         WEBEMBEDDED(WebEmbedded::new),
-        MWEB(MWeb::new);
-        TVHTML5_SIMPLY(TvHtml5Simply::new);
+        MWEB(MWeb::new),
+        TVHTML5_SIMPLY(TvHtml5SimplyWithThumbnail::new);
 
         private final ClientWithOptions<Client> clientFactory;
 
